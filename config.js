78040e26d68608f5ab75861b24a2ec690bc69cd9
AzzaLah <dimasmaulanahidayatullah85@gmail.com>
2022-03-20 19:03:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,26 +1,18 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/IBNqDyVCKgGKdbobe6vYuL', 'https://chat.whatsapp.com/JyXDLkYaxNuAwYW5kOpXEj',  'https://chat.whatsapp.com/Du7AYoa9rlI5uCH1WuMeTq']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
-    isDev: false, // if true this number will send if bot have bug and developer mode true
+  '6288299348390': { // put your number here
+    name: 'Aza [Dev]',
+    isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
-    isDev: true, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
-  },
-  '6282256115584': { // put your number here
-    name: 'Dave',
+  '6289527321564': { // put your number here
+    name: 'DepanBot [BOT]',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
-  },
-  '6285713964963': {
-    isReport: true // if true this number will send if user use command /report or /join
   },
   '13479805233': {} // ignore if you don't want anything, like name dll
 }
@@ -52,8 +44,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6289527321564'
+global.author = 'DepanBot Bot Penurut (≧▽≦)'
 
 global.multiplier = 39 // The higher, The harder levelup
 
